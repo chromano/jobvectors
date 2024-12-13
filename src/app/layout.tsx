c19972e297b003098a93a77ddb2d4a37af5db44b
Carlos H Romano <chromano@gmail.com>
2024-12-13 12:58:38 -0300

    Update layout to include the new sidebar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Quicksand } from "next/font/google";
+import Sidebar from "@/components/sidebar";
 import "./globals.css";
 
 const nunitoSans = Quicksand({
@@ -19,7 +20,16 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${nunitoSans.variable} antialiased`}>{children}</body>
+            <body className={`${nunitoSans.variable} antialiased`}>
+                <div className="w-full lg:w-4/5 mx-auto font-[family-name:var(--font-geist-sans)]">
+                    <div className="flex flex-1 flex-row gap-2">
+                        <div className="hidden md:flex w-2/5 lg:w-1/3 h-screen sticky top-0 mx-2">
+                            <Sidebar />
+                        </div>
+                        <main className="w-full mt-8">{children}</main>
+                    </div>
+                </div>
+            </body>
         </html>
     );
 }
