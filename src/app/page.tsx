2a1602fbfe08bf00a9ca675de17fda1ebd609fc8
Carlos H Romano <chromano@gmail.com>
2024-12-13 12:58:04 -0300

    Update dashboard page and remove logo, as it is part of the sidebar now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
     return (
-        <div className="min-h-screen w-full text-center font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 items-center">
-                <Image
-                    className="dark:invert rounded m-4 animate-pulse outline outline-purple-800"
-                    src="/silhouette.svg"
-                    alt="Jobvectors logo"
-                    width={200}
-                    height={200}
-                    priority
-                />
-                <div className="w-full font-[family-name:var(--font-nunito-sans)]">
-                    <p className="text-4xl mx-auto font-semibold text-purple-800">Hello world!</p>
-                </div>
-            </main>
+        <div className="w-full font-[family-name:var(--font-nunito-sans)]">
+            <p className="text-4xl mx-auto font-semibold text-purple-800">Hello world!</p>
         </div>
     );
 }
