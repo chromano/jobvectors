3bd3fee17733c19065c2de6980395a8b03c6d7a9
Carlos H Romano <chromano@gmail.com>
2024-12-14 15:39:55 -0300

    Implement Applied jobs page

--- src/app/jobs/applied/page.tsx
+++ src/app/jobs/applied/page.tsx
@@ -1,7 +1,37 @@
-export default function AppliedJobsPage() {
+import Loading from "@/components/loading";
+import Matches from "@/components/matches";
+import { Suspense } from "react";
+import { createClient } from "@/lib/supabase/server";
+import Pagination from "@/components/pagination";
+
+const ITEMS_PER_PAGE = 30;
+
+export default async function AppliedJobsPage({
+    searchParams,
+}: {
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+    const supabase = await createClient();
+    const params = await searchParams;
+    const page = params.page ? parseInt(params.page as string, 10) : 0;
+    const matches = supabase
+        .from("matches")
+        .select("*, job:jobs!inner(*)", { count: "exact" })
+        .eq("resume_id", 72)
+        .eq("dismissed", false)
+        .eq("applied", true)
+        .order("score", { ascending: false })
+        .order("id", { ascending: true })
+        .range(page * ITEMS_PER_PAGE, (page + 1) * ITEMS_PER_PAGE - 1);
+
     return (
         <div>
-            <p>Jobs listing</p>
+            <Suspense fallback={<Loading />}>
+                <Matches matches={matches} />
+                <div className="mt-4">
+                    <Pagination itemsPerPage={ITEMS_PER_PAGE} items={matches} page={page} />
+                </div>
+            </Suspense>
         </div>
     );
 }
