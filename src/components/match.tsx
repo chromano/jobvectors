4c8993cc4205b6d1cf69be053ec6122cc9a3650d
Carlos H Romano <chromano@gmail.com>
2024-12-14 12:53:13 -0300

    Complete match removal implementation

--- src/components/match.tsx
+++ src/components/match.tsx
@@ -1,10 +1,39 @@
+"use client";
 import type { Match } from "@/lib/definitions";
 import Score from "./score";
+import {
+    TrashIcon,
+    StarIcon,
+    PaperAirplaneIcon,
+    PlusCircleIcon,
+} from "@heroicons/react/24/outline";
 
-export default function Match({ match }: { match: Match }) {
+export default function Match({
+    match,
+    onMatchShortlisted,
+    onMatchApplied,
+    onMatchRemoval,
+}: {
+    match: Match;
+    onMatchShortlisted: (match: Match) => void;
+    onMatchApplied: (match: Match) => void;
+    onMatchRemoval: (match: Match) => void;
+}) {
+    const toggleShortlist = () => {
+        onMatchShortlisted(match);
+    };
+
+    const toggleApplied = () => {
+        onMatchApplied(match);
+    };
+
+    const removeMatch = () => {
+        onMatchRemoval(match);
+    };
+
     return (
         <>
-            <div className="group flex flex-col flex-1 h-full bg-white rounded border border-gray-200 p-4 transition-shadow duration-300 hover:border-purple-800 hover:outline hover:outline-purple-800">
+            <div className="group flex flex-col flex-1 h-full bg-white rounded border border-gray-200 p-4 hover:border-purple-800 hover:outline hover:outline-purple-800">
                 <div className="flex justify-between items-start">
                     <h2 className="text-md font-bold text-gray-800 whitespace-nowrap text-ellipsis overflow-hidden">
                         {match.job.role}
@@ -12,11 +41,43 @@ export default function Match({ match }: { match: Match }) {
                     <Score score={match.score} />
                 </div>
 
-                <div className="mb-4">
+                <div className="">
                     <p className="text-gray-600 font-medium whitespace-nowrap text-ellipsis overflow-hidden">
                         {match.job.company}{" "}
                         <span className="text-sm text-gray-500">{match.job.location}</span>
                     </p>
+                </div>
+
+                <div className="group flex flex-row gap-x-2 mt-2">
+                    <div className="flex flex-row gap-x-2">
+                        <button onClick={() => removeMatch()}>
+                            <TrashIcon className="w-4 h-4 text-gray-400 hover:text-purple-800" />
+                        </button>
+                        <button onClick={() => toggleShortlist()}>
+                            <StarIcon
+                                className={
+                                    "w-4 h-4 hover:text-purple-800 " +
+                                    (match.shortlisted ? "text-purple-800" : "text-gray-400")
+                                }
+                            />
+                        </button>
+                        <button onClick={() => toggleApplied()}>
+                            <PaperAirplaneIcon
+                                className={
+                                    "w-4 h-4 hover:text-purple-800 " +
+                                    (match.applied ? "text-purple-800" : "text-gray-400")
+                                }
+                            />
+                        </button>
+                    </div>
+                    <div className="flex-1 text-right">
+                        <button
+                            className={
+                                "border border-gray-100 text-gray-400 group-hover:border-transparent group-hover:text-white group-hover:bg-purple-800 text-sm px-1.5 py-0.5 rounded font-semibold"
+                            }>
+                            <PlusCircleIcon className="w-4 h-4 inline-block -mt-0.5" /> Details
+                        </button>
+                    </div>
                 </div>
             </div>
         </>
--- src/components/matches.tsx
+++ src/components/matches.tsx
@@ -1,16 +1,88 @@
 "use client";
-import { use } from "react";
+import { use, useEffect, useState } from "react";
 import Match from "./match";
 import { MatchQueryResultWithCount, Match as MatchType } from "@/lib/definitions";
+import { createClient } from "@/lib/supabase/client";
+import { Description, DialogTitle } from "@headlessui/react";
+import Modal from "./modal";
+import { Button, SecondaryButton } from "./button";
+
+const RemovalConfirmation = ({
+    isOpen,
+    onClose,
+    match,
+}: {
+    isOpen: boolean;
+    onClose: (match: MatchType | null) => void;
+    match: MatchType | null;
+}) => (
+    <Modal isOpen={isOpen} onClose={() => onClose(null)}>
+        <DialogTitle className="font-bold">Match Removal</DialogTitle>
+        <Description>
+            This will permanently remove the match. You can always keep a shortlist of relevant
+            matches instead of deleting irrelevant ones.
+        </Description>
+        <div className="flex justify-end gap-4">
+            <Button onClick={() => onClose(null)}>Cancel</Button>
+            <SecondaryButton onClick={() => onClose(match)}>Remove</SecondaryButton>
+        </div>
+    </Modal>
+);
 
 export default function Matches({ matches }: { matches: any }) {
     const entries = use<MatchQueryResultWithCount>(matches);
+    const [matchList, setMatchList] = useState<MatchType[]>([]);
+    const [confirmRemoval, setConfirmRemoval] = useState<MatchType | null>(null);
+
+    const supabase = createClient();
+
+    useEffect(() => {
+        setMatchList(entries?.data);
+    }, [entries]);
+
+    const onMatchShortlisted = async (match: MatchType) => {
+        match.shortlisted = !match.shortlisted;
+
+        await supabase
+            .from("matches")
+            .update({ shortlisted: match.shortlisted })
+            .eq("id", match.id);
+        setMatchList([...matchList]);
+    };
+
+    const onMatchApplied = async (match: MatchType) => {
+        match.applied = !match.applied;
+
+        await supabase.from("matches").update({ applied: match.applied }).eq("id", match.id);
+        setMatchList([...matchList]);
+    };
+
+    const onRemovalConfirmed = async (match: MatchType) => {
+        setConfirmRemoval(null);
+        await supabase.from("matches").update({ dismissed: true }).eq("id", match.id);
+        setMatchList(matchList.filter((m) => m.id !== match.id));
+    };
 
     return (
         <div className="flex flex-col gap-y-4">
+            <RemovalConfirmation
+                match={confirmRemoval}
+                isOpen={!!confirmRemoval}
+                onClose={(confirm) =>
+                    confirm && confirmRemoval
+                        ? onRemovalConfirmed(confirmRemoval)
+                        : setConfirmRemoval(null)
+                }
+            />
             <div className={`container grid grid-cols-1 md:grid-cols-2 gap-4 z-0`}>
-                {entries?.data?.map((match: MatchType) => (
-                    <Match match={match} key={match.id} />
+                {matchList.map((match: MatchType) => (
+                    <Match
+                        match={match}
+                        key={match.id}
+                        onMatchShortlisted={onMatchShortlisted}
+                        onMatchApplied={onMatchApplied}
+                        onMatchRemoval={setConfirmRemoval}
+                    />
                 ))}
             </div>
         </div>
