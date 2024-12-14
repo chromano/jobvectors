c1de447dc216f95b53829cebd5d5c8aea78a3582
Carlos H Romano <chromano@gmail.com>
2024-12-14 11:25:58 -0300

    Add a Modal component

---
+++ src/components/modal.tsx
@@ -0,0 +1,25 @@
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { AnimatePresence, motion } from "framer-motion";
+
+export default function Modal({ children, onClose, isOpen }) {
+    return (
+        <AnimatePresence>
+            <Dialog open={isOpen} onClose={() => onClose(false)} className="relative z-50">
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className="fixed inset-0 bg-black/30"
+                />
+
+                <div className="fixed -top-40 inset-0 flex w-screen items-center justify-center p-4">
+                    <DialogPanel
+                        as={motion.div}
+                        className="max-w-lg space-y-8 rounded border-2 border-purple-800 bg-white p-4">
+                        {children}
+                    </DialogPanel>
+                </div>
+            </Dialog>
+        </AnimatePresence>
+    );
+}
