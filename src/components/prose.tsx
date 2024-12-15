a8aa4d9616ca266608adc7f774b130505069dc56
Carlos H Romano <chromano@gmail.com>
2024-12-15 07:11:34 -0300

    Add Prose component

---
+++ src/components/prose.tsx
@@ -0,0 +1,17 @@
+import Markdown from "react-markdown";
+import remarkBreaks from "remark-breaks";
+
+export default function Prose({ children }) {
+    return (
+        <>
+            <div
+                className="prose 
+        prose-h1:font-bold prose-headings:text-md prose-headings:text-base
+        prose-headings:mb-2 prose-headings:mt-1 prose-p:my-2 prose-p:leading-tight
+        prose-a:text-blue-600 prose-p:text-justify prose-img:rounded-xl
+        prose-li:my-0.5 min-w-full">
+                <Markdown remarkPlugins={[remarkBreaks]}>{children}</Markdown>
+            </div>
+        </>
+    );
+}
