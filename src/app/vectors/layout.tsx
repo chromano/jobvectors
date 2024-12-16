fe24d90585dcf860a9e4d7c6d03f58ed47280a80
Carlos H Romano <chromano@gmail.com>
2024-12-16 11:50:04 -0300

    Load user cache right when the page is loaded

--- src/app/vectors/layout.tsx
+++ src/app/vectors/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import Sidebar from "@/components/sidebar";
+import { createClient } from "@/lib/supabase/server";
+import { getUser } from "@/lib/data/user";
 import "../globals.css";
 
 export const metadata: Metadata = {
@@ -12,13 +14,16 @@ export default async function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const supabase = await createClient();
+    const user = await getUser(supabase);
+
     return (
         <html lang="en">
             <body>
                 <div className="w-full lg:w-4/5 mx-auto font-[family-name:var(--font-geist-sans)]">
                     <div className="flex flex-1 flex-row gap-2">
                         <div className="hidden md:flex w-2/5 lg:w-1/3 h-screen sticky top-0 mx-2">
-                            <Sidebar />
+                            <Sidebar user={user} />
                         </div>
                         <main className="w-full mt-4">{children}</main>
                     </div>
