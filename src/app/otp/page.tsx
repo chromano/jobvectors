e3f4f78278786bd2870843fda8cbfbac03bad239
Carlos H Romano <chromano@gmail.com>
2024-12-24 10:44:43 -0300

    Attempt to fix login by dropping a verifyOtp call to supabase

--- src/app/otp/page.tsx
+++ src/app/otp/page.tsx
@@ -1,8 +1,14 @@
 import { redirect } from "next/navigation";
 import { createClient } from "@/lib/supabase/server";
 
-export default async function Otp() {
+export default async function Otp({ searchParams }: { searchParams: URLSearchParams }) {
     const supabase = await createClient();
+
+    await supabase.auth.verifyOtp({
+        token_hash: searchParams.get("code")!,
+        email: searchParams.get("email")!,
+        type: "email",
+    });
 
     supabase.auth.getUser().then((response) => {
         if (response.data?.user) {
